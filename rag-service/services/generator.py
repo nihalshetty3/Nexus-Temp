ec0af2ee81edff78b56d696540dc6e53cd84e346
Nihal Shetty <nihalhshetty30@gmail.com>
2026-08-02 20:33:42 +0530

    feat:Added the response generator

---
+++ rag-service/services/generator.py
@@ -0,0 +1,43 @@
+import os 
+from google import genai
+from services.retriever import retrieve
+
+client = genai.Client(
+    api_key = os.getenv("GEMINI_API_KEY")
+)
+
+def ask_rag(question):
+    retrieved_chunks = retrieve(question)
+    
+    context=""
+    
+    for i , chunk in enumerate(retrieved_chunks , 1):
+        
+        context += f"""
+Document {i}
+Source: {chunk.metadata["source"]}
+{chunk.page_content}
+----------------------------------------
+
+"""
+
+    prompt = f"""
+You are an Enterprise AI assistant.
+Answer ONLY using the retrieved context.
+If the answer is not available in the context,
+reply:
+"I couldn't find relevant information in the enterprise knowledge base."
+
+====================
+
+Context
+{context}
+
+====================
+Give a professional answer.
+"""
+    response = client.models.generate_content(
+         model="models/gemini-3.6-flash",
+        contents=prompt
+    )
+    return response.text 
