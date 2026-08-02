0be18bc92dd120b3844f2aea2f434e86451ce865
Nihal Shetty <nihalhshetty30@gmail.com>
2026-08-02 20:48:11 +0530

    Updated the RAG service with FastAPI endpoint

--- rag-service/services/generator.py
+++ rag-service/services/generator.py
@@ -1,7 +1,9 @@
 import os 
+from dotenv import load_dotenv
 from google import genai
 from services.retriever import retrieve
 
+load_dotenv()
 client = genai.Client(
     api_key = os.getenv("GEMINI_API_KEY")
 )
